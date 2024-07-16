e575d43396eb16ab58b379c5d69f92348edddaad
MkaGodh <fenryrvla@yandex.ru>
2024-07-16 17:34:18 +0300

    Добавьте файлы проекта.

---
+++ fractionMixer/fractionMixer.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string wholePart;
+	std::string fractionalPart;
+	std::string fullNum;
+
+	std::cout << "Input whole Part: ";
+	std::cin >> wholePart;
+	fullNum += wholePart + '.';
+	std::cout << "Input fractional part: ";
+	std::cin >> fractionalPart;
+	fullNum += fractionalPart;
+	double num = std::stod(fullNum);
+
+	std::cout << "Combined number: " << num;
+
+}
